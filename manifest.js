21fa8e653c9e818eb0e3a3eb4dcac722e08d6a33
Ilker Sahin <ilkzz@hotmail.com>
2018-06-04 22:25:10 +0100

    Upload

---
+++ manifest.js
@@ -0,0 +1,26 @@
+FT.manifest({
+    "filename": "index.html",
+    "width": 300,
+    "height": 600,
+    "clickTagCount": 1,
+    "hideBrowsers": ["ie9"],
+    "instantAds": [
+        { "name": "destinationName1", "type": "text", "default": "sT KILDA, VIC" },
+        { "name": "destinationName2", "type": "text", "default": "PHILLIP ISLAND, VIC" },
+        { "name": "destinationName3", "type": "text", "default": "SYDNEY HARBOUR, NSW" },
+        { "name": "destinationMain1", "type": "text", "default": "EXPERIENCE A RELAXING SUNSET STROLL" },
+        { "name": "destinationMain2", "type": "text", "default": "GET FRONT ROW SEATS TO THE WORLDS SMALLEST PARADE" },
+        { "name": "destinationMain3", "type": "text", "default": "DIG YOUR TOES<br>INTO SUN SOAKED SAND" },
+        { "name": "destinationSecondary1", "type": "text", "default": "Melbourne to sydney coaster drive from £xxx" },
+        { "name": "destinationSecondary2", "type": "text", "default": "Melbourne to sydney coaster drive from £xxx" },
+        { "name": "destinationSecondary3", "type": "text", "default": "Melbourne to sydney coaster drive from £xxx" },
+        { "name": "finalFrameText", "type": "text", "default": "MELBOURNE TO SYDNEY COASTAL DRIVE" },
+        { "name": "finalFramePrice", "type": "text", "default": "14 DAYS FROM £XXX*" },
+        { "name": "terms", "type": "text", "default": "*T&Cs apply" },
+        { "name": "cta", "type": "text", "default": "Discover more" },
+        { "name": "img1", "type": "image", "default": "img/set1_f1_300x600.png" },
+        { "name": "img2", "type": "image", "default": "img/set1_f2_300x600.png" },
+        { "name": "img3", "type": "image", "default": "img/set1_f3_300x600.png" },
+        { "name": "clickThrough", "type": "text", "default": "a" }
+    ]
+});
